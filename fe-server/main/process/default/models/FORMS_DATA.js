dce3a65f2bb7c09b8bb54eefd5d2e7e52c15f633
Chirag <Chirag2120787@gmail.com>
2018-07-23 16:21:25 +0530

    nas

---
+++ fe-server/main/process/default/models/FORMS_DATA.js
@@ -0,0 +1,50 @@
+/* jshint indent: 1 */
+const Sequelize = require('sequelize');
+module.exports =  class FeFormsData extends Sequelize.Model {
+	static init(sequelize, DataTypes) {
+	  return super.init( {
+		ID: {
+			type: Sequelize.INTEGER(10),
+            allowNull: false,
+            autoIncrement:true,
+			defaultValue: '0',
+			primaryKey: true
+		},
+		FORM_CODE: {
+            type: Sequelize.INTEGER(7),
+			allowNull: true
+		},
+		LABEL: {
+			type: Sequelize.STRING(20),
+			allowNull: true
+		},
+		JSON: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		},
+		attribute1: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		},
+		attribute2: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		},
+		attribute3: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		},
+		attribute4: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		},
+		attribute5: {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		}
+	}, {
+        tableName: 'FORMS_DATA',
+        sequelize
+	});
+}
+};
